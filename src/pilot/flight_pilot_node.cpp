7498ae100b9ea9515f8279088e26b69a90684840
huynhducmink <cnhhuynhmink@gmail.com>
2022-08-10 16:23:36 +0700

    init commit

---
+++ src/pilot/flight_pilot_node.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+#include "flightros/pilot/flight_pilot.hpp"
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "flight_pilot");
+  flightros::FlightPilot pilot(ros::NodeHandle(), ros::NodeHandle("~"));
+  return 0;
+}
