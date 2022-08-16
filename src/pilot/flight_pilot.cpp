fee8f0d16f07f4a1b0dc014134d7365c863dfa98
huynhducmink <cnhhuynhmink@gmail.com>
2022-08-16 21:48:39 +0700

    update config used in paper

--- src/pilot/flight_pilot.cpp
+++ src/pilot/flight_pilot.cpp
@@ -29,8 +29,9 @@ FlightPilot::FlightPilot(const ros::NodeHandle &nh, const ros::NodeHandle &pnh)
   rgb_pub = it.advertise("/rgb", 1);
   rgb2_pub = it.advertise("/rgb2", 1);
   depth_pub = it.advertise("/depth", 1);
+  // segmentation_pub = it.advertise("/segmentation", 1);
+  // opticalflow_pub = it.advertise("/opticalflow", 1);
 
-  //when changing camera placement, please also change the camera link transformation in the launch file to match the depth map with global map
   Vector<3> B_r_BC(0,-0.05,0.8);
   Vector<3> B_r_BC2(0,0.05,0.8);
   Matrix<3, 3> R_BC = Quaternion(0.7071068, 0, 0, -0.7071068).toRotationMatrix();
@@ -38,13 +39,13 @@ FlightPilot::FlightPilot(const ros::NodeHandle &nh, const ros::NodeHandle &pnh)
   Matrix<3, 3> R_BC2 = Quaternion(0.7071068, 0, 0, -0.7071068).toRotationMatrix();
 
   rgb2_camera_->setFOV(90);
-  rgb2_camera_->setWidth(640);
-  rgb2_camera_->setHeight(360);
+  rgb2_camera_->setWidth(720);
+  rgb2_camera_->setHeight(480);
   rgb2_camera_->setRelPose(B_r_BC2, R_BC2);
 
   rgb_camera_->setFOV(90);
-  rgb_camera_->setWidth(640);
-  rgb_camera_->setHeight(360);
+  rgb_camera_->setWidth(720);
+  rgb_camera_->setHeight(480);
   rgb_camera_->setRelPose(B_r_BC, R_BC);
   rgb_camera_->setPostProcesscing(
   std::vector<bool>{true, false, false});
@@ -57,8 +58,8 @@ FlightPilot::FlightPilot(const ros::NodeHandle &nh, const ros::NodeHandle &pnh)
 
 
   // initialize subscriber call backs
-  //sub_state_est_ = nh_.subscribe("/mavros/local_position/pose", 1,
-  sub_state_est_ = nh_.subscribe("/gazebo_groundtruth_posestamped", 1,
+  sub_state_est_ = nh_.subscribe("/mavros/local_position/pose", 1 ,
+  //sub_state_est_ = nh_.subscribe("/gazebo_groundtruth_posestamped", 1,
                                  &FlightPilot::poseCallback, this);
   // wait until the gazebo and unity are loaded
   ros::Duration(5.0).sleep();
@@ -72,8 +73,8 @@ FlightPilot::FlightPilot(const ros::NodeHandle &nh, const ros::NodeHandle &pnh)
 FlightPilot::~FlightPilot() {}
 
 void FlightPilot::poseCallback(const geometry_msgs::PoseStamped &msg) {
-  ros::Time timestamp = ros::Time::now();
-
+  //ros::Time timestamp = ros::Time::now();
+  ros::Time timestamp = msg.header.stamp;
   quad_state_.x[QS::POSX] = (Scalar)msg.pose.position.x;
   quad_state_.x[QS::POSY] = (Scalar)msg.pose.position.y;
   quad_state_.x[QS::POSZ] = (Scalar)msg.pose.position.z;
@@ -82,29 +83,32 @@ void FlightPilot::poseCallback(const geometry_msgs::PoseStamped &msg) {
   quad_state_.x[QS::ATTY] = (Scalar)msg.pose.orientation.y;
   quad_state_.x[QS::ATTZ] = (Scalar)msg.pose.orientation.z;
   quad_ptr_->setState(quad_state_);
+  
   if (unity_render_ && unity_ready_) {
     unity_bridge_ptr_->getRender(0);
     unity_bridge_ptr_->handleOutput();
   }
 
   cv::Mat img;
+  
   rgb_camera_->getRGBImage(img);
   sensor_msgs::ImagePtr rgb_msg =
     cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg();
   rgb_msg->header.stamp = timestamp;
-  rgb_pub.publish(rgb_msg);
-
+  
   rgb2_camera_->getRGBImage(img);
   sensor_msgs::ImagePtr rgb2_msg =
     cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg();
   rgb2_msg->header.stamp = timestamp;
-  rgb2_pub.publish(rgb2_msg);
-
+  
   rgb_camera_->getDepthMap(img);
   sensor_msgs::ImagePtr depth_msg =
     cv_bridge::CvImage(std_msgs::Header(), "32FC1", img).toImageMsg();
   depth_msg->header.stamp = timestamp;
   depth_msg->header.frame_id = "/camera_link";
+
+  rgb_pub.publish(rgb_msg);
+  rgb2_pub.publish(rgb2_msg);
   depth_pub.publish(depth_msg);
 }
 
